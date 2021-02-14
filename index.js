e307542a6c735af24acbff4f50453f458ff287a0
Kooroshh <mr.oplus@yahoo.fr>
2021-02-14 03:46:21 +0330

    cdn socketio for zeit

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ Message = { User : User , Message : String }
 */
 //endregion
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname+ '/node_modules'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
